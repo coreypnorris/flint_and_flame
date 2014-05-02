8baf4773b45d1dc2c09541a99ff9177d5fa4c9a2
Corey Norris <coreypnorris@gmail.com>
2014-05-02 11:31:29 -0700

    create index controller and some properties

--- js/app.js
+++ js/app.js
@@ -6,3 +6,11 @@ App.Router.map(function() {
   this.route('about');
   this.route('credits', { path: '/thanks' });
 });
+
+App.IndexController = Ember.Controller.extend({
+  productsCount: 3,
+  logo: 'images/logo-small.png',
+  time: function(){
+    return (new Date()).toDateString();
+  }.property()
+});
