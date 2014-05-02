c08a1a52d530be9264bf369eb03831c18731190d
Corey Norris <coreypnorris@gmail.com>
2014-05-02 16:33:00 -0700

    create resource route for individual contacts with a dynamic segment matching the name attribute

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ App.Router.map(function() {
   this.resource('products');
   this.resource('product', { path: '/products/:title' });
   this.resource('contacts');
+  this.resource('contact', { path: '/contacts/:name' });
 });
 
 App.IndexController = Ember.Controller.extend({
