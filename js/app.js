61da975f1e25c56dbe14c15e306ade6a6ace9f00
Corey Norris <coreypnorris@gmail.com>
2014-05-03 13:45:07 -0700

    create hasMany association on product model

--- js/app.js
+++ js/app.js
@@ -64,7 +64,8 @@ App.Product = DS.Model.extend({
   price: DS.attr('number'),
   description: DS.attr('string'),
   isOnSale: DS.attr('boolean'),
-  image: DS.attr('string')
+  image: DS.attr('string'),
+  reviews: DS.hasMany('review', {async: true})
 });
 
 App.Product.FIXTURES = [
