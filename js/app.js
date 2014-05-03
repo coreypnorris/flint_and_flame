60e731973547aae3bd5bd4be416310e4d0e598d7
Corey Norris <coreypnorris@gmail.com>
2014-05-03 12:15:30 -0700

    refactor App.PRODUCTS into fixtures

--- js/app.js
+++ js/app.js
@@ -61,8 +61,9 @@ App.Product = DS.Model.extend({
   image: DS.attr('string')
 });
 
-App.PRODUCTS = [
+App.Product.FIXTURES = [
   {
+    id: 1,
     title: 'Flint',
     price: 99,
     description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
@@ -70,6 +71,7 @@ App.PRODUCTS = [
     image: 'images/products/flint.png'
   },
   {
+    id: 2,
     title: 'Kindling',
     price: 249,
     description: 'Easily combustible small sticks or twigs used for starting a fire.',
